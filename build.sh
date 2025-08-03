330991e699f7dcaf61d4d8ac8d3275e1f332cccc
chubus <chubutrabajo@gmail.com>
2025-08-03 13:24:51 -0300

    asegurar permisos de build.sh

--- build.sh
+++ build.sh
@@ -1,12 +1,3 @@
 #!/usr/bin/env bash
-# Exit on first error
-set -e
-
-# Actualiza pip y setuptools
-pip install --upgrade pip setuptools
-
-# Instala gunicorn de forma explícita antes de las demás dependencias
-pip install gunicorn
-
-# Instala las demás dependencias del archivo requirements.txt
+pip install --upgrade pip setuptools wheel
 pip install -r requirements.txt
