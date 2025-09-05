98a71650c93b959dc9e6e2d7765e813fec21b59a
chubus <chubutrabajo@gmail.com>
2025-09-04 23:25:01 -0300

    solucion devops v1

--- venv/Scripts/activate.fish
+++ venv/Scripts/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV 'C:\Users\rey_a\Documents\Belgrano_ahorro\venv'
+set -gx VIRTUAL_ENV 'C:\Users\rey_a\Documents\Belgrano_ahorro-back\venv'
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"Scripts $PATH
